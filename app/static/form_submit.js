e4fcdc836c022c2199c24a1be08d68d9227136d6
Nandhini360 <nandhinisivaraj.mit@gmail.com>
2026-02-27 14:52:10 +0530

    Enhanced Prticipant Information

--- app/static/form_submit.js
+++ app/static/form_submit.js
@@ -1,89 +1,99 @@
 $(document).ready(function() {
+
+    /* -----------------------------
+       REGISTER FORM SUBMISSION
+    ----------------------------- */
+
+    function makeLinksClickable(text) {
+        const urlPattern = /(https?:\/\/[^\s]+)/g;
+        return text.replace(urlPattern, function(url) {
+            return `<a href="${url}" target="_blank" style="color:yellow;font-weight:bold;">
+                        ${url}
+                    </a>`;
+        });
+    }
+
     $('#modal-form').on('submit', function(event) {
         event.preventDefault();
+
         $('#msg').html('Please Wait!!!');
+
         $.ajax({
             url: REGISTER_URL,
             type: 'POST',
-            data:{
-                "id":$('#event-id').val(),
-                "reg1":$('#reg1').val(),
-                "reg2":$('#reg2').val(),
-                "reg3":$('#reg3').val(),
-                "reg4":$('#reg4').val(),
-                "reg5":$('#reg5').val(),
-                'type':EVENT_CATEGORY
+            data: {
+                "id": $('#event-id').val(),
+                "reg1": $('#reg1').val(),
+                "reg2": $('#reg2').val(),
+                "reg3": $('#reg3').val(),
+                "reg4": $('#reg4').val(),
+                "reg5": $('#reg5').val(),
+                "type": EVENT_CATEGORY
             }
         })
-        .done(function(data){
-            if(data.error) {
+        .done(function(data) {
+
+            $('#register-modal').modal('hide');
+
+            if (data.error) {
+
                 $('#msg').html(data.error);
                 $('#register-modal').modal('hide');
                 $('#alert-modal').modal('show');
-                // console.log(data.error);
-            }
-            else {
-                $('#msg').html(data.success);
-                $('#register-modal').modal('hide');
+
+            } else {
+
+                let content = PARTICIPANT_INFO && PARTICIPANT_INFO.trim() !== "" 
+                    ? makeLinksClickable(PARTICIPANT_INFO).replace(/\n/g, "<br>")
+                    : data.success;
+
+                $('#msg').html(`
+                    <h5 style="color:yellow;">Registration Successful 🎉</h5>
+                    <hr/>
+                    ${content}
+                `);
+
                 $('#alert-modal').modal('show');
             }
+
+        })
+        .fail(function() {
+            $('#register-modal').modal('hide');
+            $('#msg').html("Something went wrong. Please try again.");
+            $('#alert-modal').modal('show');
         });
     });
 
-    // $("#rowAdder").click(function () {
-    //     newRowAdd =
-    //     '<div id="row"> <div class="input-group m-3">' +
-    //     '<div class="input-group-prepend">' +
-    //     '<button class="btn btn-danger" id="DeleteRow" type="button">' +
-    //     '<i class="bi bi-trash"></i> Delete</button> </div>' +
-    //     '<input type="text" class="form-control m-input"> </div> </div>';
 
-    //     $('#modal-form').append(newRowAdd);
-    // });
+    /* -----------------------------
+       DELETE TEAM MEMBER ROW
+    ----------------------------- */
 
     $("body").on("click", "#DeleteRow", function () {
         $(this).parents("#row").remove();
     });
 
 
-    $("add-event-form").on('submit', function(event) {
+    /* -----------------------------
+       ADD EVENT FORM SUBMISSION
+       (Corrected Selector + Single Handler)
+    ----------------------------- */
+
+    $('#add-event-form').on('submit', function(event) {
         event.preventDefault();
 
         var formData = $(this).serialize();
-        var url = $('input[type="submit"]').data('url');
+        var url = $(this).attr('action');
 
         $.ajax({
-            url: url, // URL to submit form data
+            url: url,
             type: 'POST',
             data: formData,
             success: function(response) {
                 alert('Form data submitted successfully!');
                 // do something with response
             },
-            error: function(jqXHR, textStatus, errorThrown) {
-                // console.log(textStatus, errorThrown);
-                alert(textStatus);
-                // handle error
-            }
-        });
-    });
-
-    $("add-event-form").on('submit', function(event) {
-        event.preventDefault();
-
-        var formData = $(this).serialize();
-        var url = $('input[type="submit"]').data('url');
-
-        $.ajax({
-            url: url, // URL to submit form data
-            type: 'POST',
-            data: formData,
-            success: function(response) {
-                alert('Form data submitted successfully!');
-                // do something with response
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                // console.log(textStatus, errorThrown);
+            error: function(jqXHR, textStatus) {
                 alert(textStatus);
                 // handle error
             }
