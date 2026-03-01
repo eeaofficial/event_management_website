9481e95ef244a053290daea99ab48d18150a7ebb
Nandhini360 <nandhinisivaraj.mit@gmail.com>
2026-03-01 22:53:09 +0530

    MIT Column

--- app/utils.py
+++ app/utils.py
@@ -33,7 +33,7 @@ def is_code_applicable() -> bool:
     code_applicable = False
 
     if current_user.college == 'MIT':
-        if '201950' in reg_no or '202050' in reg_no or '202150' in reg_no or '202250' in reg_no:
+        if '202250' in reg_no or '202350' in reg_no or '202450' in reg_no or '202550' in reg_no:
             code_applicable = True
 
     return code_applicable
